eb89615fce36c8f7ab6df5896053a99946c4d58c
ibrahim180101 <144534622+ibrahim180101@users.noreply.github.com>
2026-03-11 21:46:15 +0530

    Add files via upload

---
+++ 11-03-2026 Tasks.py
@@ -0,0 +1,83 @@
+#Task 1 
+
+print("Hello World",end=" ")
+print("Welcome python")
+print("Laptop","Mouse","keyboard",sep=" | ")
+
+#Task 2
+name ="Ravi"
+age = 22
+city = "Chennai"
+print(name,age,city,sep="-")
+
+#Task 3
+Name,Age,student="Meena","20","True"
+print(Name,Age,student)
+
+#Task 4
+Word = "Python"
+print(Word[0])
+print(Word[2])
+print(Word[5])
+
+#Task 5
+print(25 + 10)
+print(50 - 20)
+print(8 * 5)
+print(100 / 10)
+print(10 % 3)
+print( 2 ** 4)
+print(20 // 3)
+
+'''Task 6
+ 3 + 2 * 5 ** 2
+first 5 ** 2 = 25
+second 2 * 25 = 50
+last 3 + 50
+'''   
+print( 3 + 2 * 5 ** 2)
+
+#Task 7
+#1 num = 50 num+=25 => num = num(50) + 25 
+num = 50 
+num +=25
+print(num)
+#2 num = 100 num/=10 => num = num(100) / 10
+Num = 100
+Num /= 10
+print(Num)
+
+#Task 8
+print(10 > 5)
+print(20 < 15)
+print(5 == 5)
+print(10 != 8)
+print(7 >= 7)
+print(6 <= 2)
+
+#Task 9
+a = "apple"
+b = "Apple"
+print(a == b) # first letter of a and b is case sensitive
+
+#Task 10
+print(10 > 5 and 5 == 5)
+print( 5 > 10 or 10 == 10)
+print(not(5>2))
+
+#Task 11
+numbers =[10,20,30,40,50]
+print(20 in numbers)
+print( 60 in numbers)
+print(30 not in numbers)
+
+#Task 12
+A = 10 
+B = 20 
+A,B= B,A
+print("A =",A,"B =",B)
+
+#Task 13
+AA = 6
+BB = 3
+print(AA ^ BB)
